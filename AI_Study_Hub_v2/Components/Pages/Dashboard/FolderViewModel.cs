fb0f3060c3b5b4b898f11370b38eaeedaec085af
LongVNT060206 <thanhlong060206@gmail.com>
2026-06-29 11:14:51 +0700

    feat: add folder click navigation from dashboard to analytics
    - FolderViewModel: added FolderId property
    - FolderTable: added OnFolderClick event callback + row click handler
    - FolderDashboard: populate FolderId, navigate to analytics on click
    - AnalyticsDashboard: accept folderId/folderName query params, show folder name in breadcrumb

--- AI_Study_Hub_v2/Components/Pages/Dashboard/FolderViewModel.cs
+++ AI_Study_Hub_v2/Components/Pages/Dashboard/FolderViewModel.cs
@@ -2,6 +2,7 @@ namespace AI_Study_Hub_v2.Components.Pages.Dashboard;
 
 public class FolderViewModel
 {
+    public Guid? FolderId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string SubjectCode { get; set; } = string.Empty;
     public string Semester { get; set; } = string.Empty;
