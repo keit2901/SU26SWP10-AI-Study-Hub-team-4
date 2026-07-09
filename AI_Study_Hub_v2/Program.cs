927554b46bc405b8a78738fa577056d9c5d79c68
keit2901 <kakalga000@gmail.com>
2026-07-09 21:46:10 +0700

    fix(review): remove duplicate IQuizService DI; wire ResolvedByUserId in EscalationService

--- AI_Study_Hub_v2/Program.cs
+++ AI_Study_Hub_v2/Program.cs
@@ -261,7 +261,6 @@ builder.Services.AddScoped<AuthSessionState>();
 builder.Services.AddScoped<AuthPersistenceService>();
 builder.Services.AddScoped<AiChatSessionState>();
 builder.Services.AddScoped<IChatPersistenceService, ChatPersistenceService>();
-builder.Services.AddScoped<IQuizService, QuizService>();
 builder.Services.AddScoped<IDashboardService, DashboardService>();
 
 // Authentication / Authorization ---------------------------------------------
